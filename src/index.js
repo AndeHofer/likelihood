1a536bef4f0b78db47d709e70e19767c7adee64f
Ande Hofer <andehofer@gmail.com>
2021-04-16 01:22:53 +0200

    added system

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+//import reportWebVitals from "./reportWebVitals";
+//import 'bootstrap/dist/css/bootstrap.css';
 
 function calculateHitChance(attack, ac, adv) {
   let result = Number(0);
@@ -15,7 +16,7 @@ function calculateHitChance(attack, ac, adv) {
         if (adv === "Advantage") {
           result = result + (1 - result) * result;
         } else if (adv === "Disadvantage") {
-          result = result - (result * (1 - result));
+          result = result - result * (1 - result);
         }
       }
     }
@@ -35,29 +36,68 @@ class RadioDisadvantage extends React.Component {
   }
 
   render() {
+    const value = this.props.value;
     return (
       <div>
         <input
           type="radio"
           value="Normal"
+          checked={value === "Normal"}
           name="adv"
           onChange={this.handleChange}
-        />{" "}
-        Normal
+        />
+        Normal{" "}
         <input
           type="radio"
           value="Advantage"
+          checked={value === "Advantage"}
           name="adv"
           onChange={this.handleChange}
-        />{" "}
-        Advantage
+        />
+        Advantage{" "}
         <input
           type="radio"
           value="Disadvantage"
           name="adv"
+          checked={value === "Disadvantage"}
           onChange={this.handleChange}
-        />{" "}
-        Disadvantage
+        />
+        Disadvantage{" "}
+      </div>
+    );
+  }
+}
+
+class RadioSystem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onValueChange(e.target.value);
+  }
+
+  render() {
+    const value = this.props.value;
+    return (
+      <div>
+        <input
+          type="radio"
+          value="DnD 5e"
+          checked={value === "DnD 5e"}
+          name="sys"
+          onChange={this.handleChange}
+        />
+        DnD 5e{" "}
+        <input
+          type="radio"
+          value="PF 2e"
+          checked={value === "PF 2e"}
+          name="sys"
+          onChange={this.handleChange}
+        />
+        PF 2e{" "}
       </div>
     );
   }
@@ -87,11 +127,12 @@ class ValueInput extends React.Component {
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { attack: "0", ac: "0", adv: "Normal" };
+    this.state = { attack: "0", ac: "10", adv: "Normal", sys: "DnD 5e" };
 
     this.handleAcChange = this.handleAcChange.bind(this);
     this.handleAttackChange = this.handleAttackChange.bind(this);
     this.handleAdvChange = this.handleAdvChange.bind(this);
+    this.handleSysChange = this.handleSysChange.bind(this);
   }
 
   handleAcChange(value) {
@@ -106,13 +147,34 @@ class Calculator extends React.Component {
     this.setState({ adv: value });
   }
 
+  handleSysChange(value) {
+    this.setState({ sys: value });
+    // if(value === "PF 2e"){
+    //   this.setState({adv : "Normal"});
+    // }
+  }
+
   render() {
     const ac = this.state.ac;
     const attack = this.state.attack;
-    const adv = this.state.adv;
+    const sys = this.state.sys;
+    let adv;
+    let advElement;
+    if (sys === "DnD 5e") {
+      adv = this.state.adv;
+      advElement = (
+        <RadioDisadvantage onValueChange={this.handleAdvChange} value={adv}  />
+      );
+    } else {
+      adv = "Normal";
+      advElement = <div>&nbsp;</div>;
+    }
     const likelihoodHit = calculateHitChance(attack, ac, adv);
+
     return (
       <div>
+        <RadioSystem onValueChange={this.handleSysChange} value={sys} />
+        <div>&nbsp;</div>
         <ValueInput
           value={attack}
           onValueChange={this.handleAttackChange}
@@ -122,9 +184,10 @@ class Calculator extends React.Component {
         <ValueInput
           value={ac}
           onValueChange={this.handleAcChange}
-          label="AC: "
+          label="AC:"
         />
-        <RadioDisadvantage onValueChange={this.handleAdvChange} value={adv} />
+        <div>&nbsp;</div>
+        {advElement}
         <p>Likelihood Hit: {likelihoodHit} %</p>
       </div>
     );
@@ -136,4 +199,4 @@ ReactDOM.render(<Calculator />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
